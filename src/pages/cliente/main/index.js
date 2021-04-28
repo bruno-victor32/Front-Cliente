3c512609c959ef8bf8e049ae469c30a24b49c504
Bruno Victor da Silva Vicente <bruno.victor32@yahoo.com.br>
2021-04-27 21:44:25 -0300

    upload and delete

--- src/pages/cliente/main/index.js
+++ src/pages/cliente/main/index.js
@@ -26,10 +26,8 @@ export default class Main extends Component {
  
         return (
             <div className="usuario-list">
-                <Link to={`/criarUsuario`}> <button type="button" class="btn btn-success">Novo</button> </Link>
+                <Link to={`/criarCliente`}> <button type="button" class="btn btn-success">Novo</button> </Link>
                 <br /><br />
- 
-                <button type="button" class="btn btn-primary">Bruno Vida Loka!</button>
  
                 <table class="table table-hover">
                     <thead>
@@ -50,9 +48,9 @@ export default class Main extends Component {
                                 <td>{usuario.salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
                                 <td>{new Date(usuario.dataNascimento).toLocaleDateString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit' })}</td>
                                 <td>{usuario.ativo ? "Sim" : "Não"}</td>
-                                <td> <Link to={`/usuarios/${usuario.id}`}> <button type="button" class="btn btn-primary">Detalhes</button> </Link> </td>
-                                <td><button type="button" class="btn btn-warning">Atualizar</button></td>
-                                <td><button type="button" class="btn btn-danger">Excluir</button></td>
+                                <td> <Link to={`/clientes/${usuario.id}`}> <button type="button" class="btn btn-primary">Detalhes</button> </Link> </td>
+                                <td> <Link to={`/editarCliente/${usuario.id}`}> <button type="button" class="btn btn-warning">Atualizar</button> </Link></td>
+                                <td> <Link to={`/deletarCliente/${usuario.id}`}> <button type="button" class="btn btn-danger">Excluir</button> </Link></td>
                             </tr>
                         ))}
                     </tbody>
